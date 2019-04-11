42720e5a55ee371ab66c11262b029f0573be0b97
Brie <brienne.mcnally@segment.com>
2019-04-11 14:53:20 -0700

    Remove orgId

--- analytics-ios-mcvid/Classes/SEGMCVIDTracker.h
+++ analytics-ios-mcvid/Classes/SEGMCVIDTracker.h
@@ -1,10 +1,20 @@
 #import <Foundation/Foundation.h>
 #import <Analytics/SEGMiddleware.h>
 
+
 @interface SEGMCVIDTracker : NSObject <SEGMiddleware>
 
 + (id<SEGMiddleware>)middleware;
+@property (nonatomic) NSString *organizationId;
 
-- (void)sendRequestAdobeExperienceCloud:(NSString *)advertiserId organizationId:(NSString *)organizationId completion:(void (^)(NSString *marketingCloudIdKey, NSError *))completion;
+- (void) sendRequestAdobeExperienceCloud:(NSString *)advertiserId organizationId:(NSString *)organizationId completion:(void (^)(NSString *marketingCloudIdKey, NSError *))completion;
 
 @end
+
+// @interface MCVIDAdobeError : NSObject
+//
+// @property (nonatomic, assign) MCVIDAdobeError *code;
+// @property (nonatomic) NSString *message;
+// @property (nonatomic) NSError *error;
+//
+// @end
--- analytics-ios-mcvid/Classes/SEGMCVIDTracker.m
+++ analytics-ios-mcvid/Classes/SEGMCVIDTracker.m
@@ -1,46 +1,86 @@
 #import "SEGMCVIDTracker.h"
 #import <Analytics/SEGAnalyticsUtils.h>
 
-@interface MCVIDAdobeError ()
-
-- (id)initWithCode:(MCVIDAdobeError)code message:(NSString *)message error:(NSError *)error;
-
-@end
-
-@implementation MCVIDAdobeError
-- (id)initWithCode:(MCVIDAdobeError)code message:(NSString *)message error:(NSError *)error {
-    self = [super  init];
-    if (self) {
-      _code = code;
-      _message = message;
-      _error = error;
-    }
-    return self;
-}
-
-- (NSString *)description {
-    NSMutableString *description = [[NSMutableString alloc] initWithString:@"MCVIDAdobeError {\n"];
-    [description appendFormat:@"code:%@\n", @(_code)];
-    [description appendFormat:@"message:%@\n", @(_message)];
-    [description appendFormat:@"error:%@\n", @(_error)];
-    [description appendString:@"}"];
-    return description;
-}
-
-@end
 
 @implementation SEGMCVIDTracker
 
-+ (id<SEGMiddleware>)middleware
-{
-    return [[SEGMCVIDTracker alloc] init];
++(id<SEGMiddleware> organizationId:(NSString *)organizationId)middleware {
+    initWithOrganizationId:(NSString *)organizationId {
+        self = [super init];
+        if (self) {
+            self.organizationId = organizationId;
+        }
+        return self
+    }
 }
+
++ (id<SEGMiddleware>)middleware {
+     [[SEGMCVIDTracker alloc] init];
+}
+
+- (void)sendRequestAdobeExperienceCloud:(NSString *)advertisingId organizationId:(NSString *)organizationId completion:(void (^)(NSString *marketingCloudId, NSError *))completion {
+    //Variables to build URL for GET request
+    NSString *protocol = @"https";
+    NSString *host = @"dpm.demdex.net";
+    NSString *path = @"/id?";
+
+    //Defaulted values for request
+    NSString *versionKey = @"d_ver"; //d_ver defaults to 2
+    NSString *version = @"2"; //d_ver defaults to 2
+    NSString *jsonFormatterKey = @"&d_rtbd";//&d_rtbd and defaults to = json
+    NSString *jsonFormatter = @"json";//&d_rtbd and defaults to = json
+    NSString *regionKey = @"dcs_region"; //dcs_region key defaults to = 6
+    NSString *region = @"6"; //dcs_region key defaults to = 6
+    NSString *marketingCloudIdKey = @"d_mid";
+    NSString *organizationIdKey = @"d_orgid"; //can retrieve from settings
+
+    //Variables for when advertising Id is present
+    NSString *deviceTypeKey = @"DSID_20915";//means ios
+    NSString *separator = @"%01";
+    NSString *advertisingIdKey = @"d_cid_ic";
+
+    //Values to build URl components and quuery items
+    NSMutableString *urlString = [NSMutableString stringWithFormat:@"%@://%@%@", protocol, host, path];
+    NSURLComponents *components = [NSURLComponents componentsWithString:urlString];
+    NSMutableArray *queryItems = [NSMutableArray array];
+
+    if (advertisingId) {
+        [queryItems addObject:[NSURLQueryItem queryItemWithName:advertisingIdKey value:[NSString stringWithFormat:@"%@%@%@", deviceTypeKey, separator, advertisingId]]];
+    }
+
+    [queryItems addObject:[NSURLQueryItem queryItemWithName:versionKey value:version]];
+    [queryItems addObject:[NSURLQueryItem queryItemWithName:jsonFormatterKey value:jsonFormatter]];
+    [queryItems addObject:[NSURLQueryItem queryItemWithName:regionKey value:region]];
+    [queryItems addObject:[NSURLQueryItem queryItemWithName:organizationIdKey value:organizationId]];
+
+    components.queryItems = queryItems;
+    NSURL *url = components.URL;
+
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+
+    [[session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        NSDictionary *dictionary = nil;
+        @try {
+            dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        } @catch (NSException *exception) {
+            // return callBackForError(MCVIDJsonParseError, @"Deserializing the JSON response failed", nil);
+            return (NSLog(@"error"));
+        }
+
+        NSString *marketingCloudId = dictionary[marketingCloudIdKey];
+        completion(marketingCloudId, nil);
+    }] resume];
+}
+
 
 - (void)context:(SEGContext *_Nonnull)context next:(SEGMiddlewareNext _Nonnull)next
 {
-    NSString *advertisingId = context.device[@"advertisingId"];
-    NSString *organizationId = context.settings[@"marketingCloudOrgId"];
     SEGIdentifyPayload *identify =(SEGIdentifyPayload *)context.payload;
+    NSString *advertisingId = identify.context[@"device"][@"advertistingId"];
+    NSString *organizationId = @"orgID@AdobeOrg"; //Segments Org Id for Testing
 
     if (context.eventType != SEGEventTypeIdentify) {
         next(context);
@@ -57,92 +97,22 @@
         return;
     }
 
-    [sendRequestAdobeExperienceCloud:advertisingId organizationId:organizationId completion:(void (^)(NSString *marketingCloudId, NSError *))completion {
+    [self sendRequestAdobeExperienceCloud:advertisingId organizationId:organizationId completion:^(NSString *marketingCloudId, NSError *error) {
       if (marketingCloudId.length) {
-        SEGContext *newContext = [context modify:^(id<SEGMutableContext> _Nonnull ctx) {
-            ctx.payload = [[SEGIdentifyPayload alloc] initWithEvent:identify.event
-                                                      properties:identify.properties
-                                                         context:identify.context
-                                                    integrations:identify.integrations.["Adobe Anlytics"].marketingCloudId ];
-
-          //somewhere here append marketingCloudId to newContext?????
-
-        }];
-        next(newContext);
+          NSLog(@"This is working!, %@", marketingCloudId);
+        // SEGContext *newContext = [context modify:^(id<SEGMutableContext> _Nonnull ctx) {
+        //     ctx.payload = [[SEGIdentifyPayload alloc] initWithEvent:identify.event
+        //                                               properties:identify.properties
+        //                                                  context:identify.context
+        //                                             integrations:identify.integrations.["Adobe Anlytics"].marketingCloudId ];
+        //
+        //   //somewhere here append marketingCloudId to newContext?????
+        //
+        // }];
+        next(context);
       }
-    }]
+    }];
 
-}
-
-- (void)sendRequestAdobeExperienceCloud:(NSString *)advertisingId organizationId:(NSString *)organizationId completion:(void (^)(NSString *marketingCloudId, NSError *))completion {
-  //Variables to build URL for GET request
-  NSString *protocol = @"https";
-  NSString *host = @"dpm.demdex.net";
-  NSString *path = @"/id?";
-
-  //Defaulted values for request
-  NSString *versionKey = @"d_ver"; //d_ver defaults to 2
-  NSString *version = @"2"; //d_ver defaults to 2
-  NSString *jsonFormatterKey = @"&d_rtbd" //&d_rtbd and defaults to = json
-  NSString *jsonFormatter = @"json" //&d_rtbd and defaults to = json
-  NSString *regionKey = @"dcs_region"; //dcs_region key defaults to = 6
-  NSString *region = @"6"; //dcs_region key defaults to = 6
-  NSString *marketingCloudIdKey =@"d_mid"
-  NSString *organizationIdKey = @"d_orgid"; //can retrieve from settings
-
-  //Variables for when advertising Id is present
-  NSString *deviceTypeKey = @"DSID_20915" //means ios
-  NSString *separator = @"%01";
-  NSString *advertisingIdKey = @"d_cid_ic";
-
-  //Error messaging
-  NSString *errorResponseKey = @"error_msg";
-  NSString *errorMessageKey = @"msg";
-  NSString *errorCodeKey = @"code";
-
-  //Values to build URl components and quuery items
-  NSMutableString *urlString = [NSMutableString stringWithFormat:@"%@://%@%@", protocol, host, path];
-  NSURLComponents *components = [NSURLComponents componentsWithString:urlString];
-  NSMutableArray *queryItems = [NSMutableArray array];
-
-  if (advertisingId) {
-    [queryItems addObject:[NSURLQueryItem queryItemWithName:advertisingIdKey value:[NSString stringWithFormat:@"%@%@%@", deviceTypeKey, separator, advertisingId]]];
-  }
-
-  [queryItems addObject:[NSURLQueryItem queryItemWithName:versionKey value:version]];
-  [queryItems addObject:[NSURLQueryItem queryItemWithName:jsonFormatterKey value:jsonFormatter]];
-  [queryItems addObject:[NSURLQueryItem queryItemWithName:regionKey value:region]];
-  [queryItems addObject:[NSURLQueryItem queryItemWithName:organizationIdKey value:organizationId]];
-
-  components.queryItems = queryItems;
-  NSURL *url = components.URL;
-
-  NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-  NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
-  NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
-
-  [[session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-
-    void (^callBackForError)(MCVIDAdobeError code, NSString *message, NSError) = ^void(MCVIDAdobeError code, NSString *message, NSError *error){
-      MCVIDAdobeError *adobeError =[[MCVIDAdobeError alloc] initWithCode:code message:message error:error ];
-      NSError *error = [NSError code:adobeError.code];
-      completion(nil, nil, nil, error);
-    };
-
-    if (error) {
-      return callBackForError(MCVIDAdobeRequestError, @"Failed", error);
-    }
-
-    NSDictionary = nil;
-    @try {
-      dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-    } @catch (NSException *exception) {
-      return callBackForError(MCVIDAdobeSerializationError, @"Deserializing the JSON response failed", nil);
-    }
-
-    NSString *marketingCloudId = dictionary[marketingCloudIdKey]
-    completion(marketingCloudOrgId, nil);
-  }] resume];
 }
 
 @end
