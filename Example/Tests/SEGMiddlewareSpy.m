e6d50c030989e0b55f81436c41947340828539d1
Xavier Jurado <xavier.jurado@gmail.com>
2019-08-02 10:01:54 +0200

    Cleanup

--- Example/Tests/SEGMiddlewareSpy.m
+++ Example/Tests/SEGMiddlewareSpy.m
@@ -1,11 +1,3 @@
-//
-//  SEGMiddlewareSpy.m
-//  analytics-ios-mcvid_Tests
-//
-//  Created by Xavier Jurado on 01/08/2019.
-//  Copyright © 2019 Brie. All rights reserved.
-//
-
 #import "SEGMiddlewareSpy.h"
 
 @interface SEGMiddlewareSpy ()
